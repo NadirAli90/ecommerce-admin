d978c630fcc5a406f6fbec6428ac576fe104cdb9
NadirAli <nadirali@NadirAlis-MacBook-Pro.local>
2023-07-17 13:03:13 -0700

    admin

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,6 +1,7 @@
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import Navbar from "@/components/navbar";
 
 export default async function DashboardLayout({
   children,
@@ -28,7 +29,7 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <div>This Will be a Navbar</div>
+      <Navbar />
       {children}
     </>
   );
